e147e1a287d00972554c04e9ff74eff69e90df4f
palakpk954 <palakpk954@gmail.com>
2026-09-22 15:04:33 +0530

    fix: replace deprecated react-tsparticles with @tsparticles/react to fix GH Action build failure

--- src/components/FireParticles.jsx
+++ src/components/FireParticles.jsx
@@ -1,16 +1,25 @@
-import React, { useCallback } from 'react';
-import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
+import React, { useCallback, useEffect, useState } from 'react';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadSlim } from '@tsparticles/slim';
 
 const FireParticles = () => {
-  const particlesInit = useCallback(async engine => {
-    await loadFull(engine);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
+
+  if (!init) {
+    return null;
+  }
 
   return (
     <Particles
       id="tsparticles"
-      init={particlesInit}
       options={{
         fullScreen: { enable: false, zIndex: 0 },
         fpsLimit: 120,
