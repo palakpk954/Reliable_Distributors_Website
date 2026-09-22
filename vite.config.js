53f27af06f664634b5ad5f99cc3f6c89896d3369
palakpk954 <palakpk954@gmail.com>
2026-09-22 10:54:42 +0530

    Fix GitHub Pages white screen (relative paths)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Reliable_Distributors_Website/',
+  base: './',
   plugins: [react()],
 })
